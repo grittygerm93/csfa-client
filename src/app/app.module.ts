95a7ef9909fef2ca538087e1024462dce9606625
Alex <alexius.lzc@gmail.com>
2022-02-11 14:57:52 +0800

    after clean up and adding pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {GuardService} from "./services/guard.service";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { TemplateComponent } from './template/template.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
-import { RecipelistComponent } from './recipelist/recipelist.component';
-import { RecipeaddComponent } from './recipeadd/recipeadd.component';
-import { RecipedetailComponent } from './recipedetail/recipedetail.component';
+import {RecipelistComponent} from './recipelist/recipelist.component';
+import {RecipeaddComponent} from './recipeadd/recipeadd.component';
+import {RecipedetailComponent} from './recipedetail/recipedetail.component';
 import {RecipeService} from "./services/recipe.service";
 import {MatListModule} from "@angular/material/list";
 import {FlexModule} from "@angular/flex-layout";
@@ -20,11 +19,12 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateComponent,
     RecipelistComponent,
     RecipeaddComponent,
     RecipedetailComponent
@@ -42,9 +42,16 @@ import {MatInputModule} from "@angular/material/input";
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [GuardService, RecipeService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -11,29 +11,21 @@ export class RecipeService {
 
 
   getAllRecipes(): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>('http://localhost:8080/api/recipes')
-    // return this.http.get<Customer>('https://csf-day6.herokuapp.com/api/customer')
+    // return this.http.get<Recipe[]>('http://localhost:8080/api/recipes')
+    return this.http.get<Recipe[]>('/api/recipes')
   }
-
-/*  fetch() {
-    this.http.get<ToDo[]>('http://localhost:8080/api/tasks')
-      .subscribe((todoArr) => {
-        console.log(JSON.stringify(todoArr));
-        this.taskChanged.next(todoArr);
-        this.dex.bulkPut(todoArr);
-      })
-  }*/
-
 
   getRecipe(recipeId: string): Observable<Recipe> {
     const url = `http://localhost:8080/api/recipe/${recipeId}`
     console.log(url);
-    return this.http.get<Recipe>(`http://localhost:8080/api/recipe/${recipeId}`)
+    // return this.http.get<Recipe>(`http://localhost:8080/api/recipe/${recipeId}`)
+    return this.http.get<Recipe>(`/api/recipe/${recipeId}`)
   }
 
   postRecipe(recipe: Recipe) {
     console.log(recipe)
-    this.http.post('http://localhost:8080/api/recipe', recipe)
+    // this.http.post('http://localhost:8080/api/recipe', recipe)
+    this.http.post('/api/recipe', recipe)
       .subscribe((res) => {
         console.log(res);
       })
